20586a2a684bdfdd7dcbfc5fe580b10d7b15893f
Lucas Lemes <112514184+LucasFuriniLemes@users.noreply.github.com>
2022-11-06 20:37:23 -0300

    Add files via upload

---
+++ Exercício.3/R3.js
@@ -0,0 +1,11 @@
+
+var n1 = prompt("Digite o primeiro número: ");
+var n2 = prompt("Digite o segundo número: ");
+    if (n1 > n2){
+      alert("O primeiro número é maior: " +n1);
+    } else if(n2 > n1) {
+      alert("O segundo número é maior: " +n2);
+      }
+      else if(n2 == n1) {
+      alert("Os números são iguais");
+      }
